8657c9f08d87a16e3bea6565bb528925bc52a6d9
mehmetusak <mehmet-usak@hotmail.com>
2022-08-04 00:36:14 +0300

    firebase_services done

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -1,6 +1,23 @@
+import 'dart:convert';
+
+import 'package:get/get_connect/http/src/status/http_status.dart';
+import 'package:http/http.dart' as http;
+import 'package:loginx/model/userModel.dart';
+
 class FirebaseServices {
-  static const String FirebaseURL = "";
-  Future getStudent() async {
-    final response = "";
+  static const String FirebaseURL =
+      "https://loginx-e2132-default-rtdb.firebaseio.com/";
+
+  Future<List<usermodel>> getUsers() async {
+    final response = await http.get(Uri.parse(FirebaseURL));
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        final userModel = json.decode(response.body);
+        final userList = userModel as List<usermodel>;
+        return userList;
+
+      default:
+        return Future.error(response.statusCode);
+    }
   }
 }
